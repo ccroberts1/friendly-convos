3203c6eb43e972fd75c2d7d6e23d40fc144cd360
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2022-01-12 18:28:46 -0700

    added virtual to user Schema

--- models/User.js
+++ models/User.js
@@ -1,26 +1,36 @@
 const { Schema, model } = require("mongoose");
-
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
+const thoughtSchema = require("./Thought");
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+        "Please provide a valid email address",
+      ],
+    },
+    thoughts: [thoughtSchema],
+    friends: {
+      //Needs to self-reference
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [
-      /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
-      "Please provide a valid email address",
-    ],
-  },
-  thoughts: {
-    //Needs to be added after thoughts model
-  },
-  friends: {
-    //Needs to self-reference
-  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
   //Also needs a virtual called friendCount that retrieves the length of the user's friends array field on query
-});
+);
+
+const User = model("user", userSchema);
+
+module.exports = User;
