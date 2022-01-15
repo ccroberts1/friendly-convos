d689a67b7d5774986492f7882686e7ae1900f9b7
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2022-01-15 10:36:04 -0700

    added routing for users and thoughts paths, started on controllers for users and thoughts

---
+++ controllers/thoughtsController.js
@@ -0,0 +1,11 @@
+const { Thought, User } = require("../models");
+
+module.exports = {
+  //Get all Thoughts
+  //Get a single Thought
+  //Create a new Thought
+  //Update a Thought
+  //Remove a Thought
+  //Create a reaction
+  //Remove a reaction
+};
---
+++ controllers/usersController.js
@@ -0,0 +1,11 @@
+const { Thought, User } = require("../models");
+
+module.exports = {
+  // Get all users
+  //Get a single user
+  //Create a user
+  //Update a user
+  //Delete a user
+  //Add a new friend to a user's friend list
+  //Remove a friend from a user's friend list
+};
